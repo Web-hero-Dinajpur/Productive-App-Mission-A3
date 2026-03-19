8798fcac4f51527c0679ba11fbf464b485adf29b
sreedhar <sreedharroy0@gmailcom>
2026-03-19 01:06:18 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <section id="center">
+       
+        <div>
+          <h1>Get started</h1>
+       
+        </div>
+        
+      </section>
+
+
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Pages/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            this is header file
+        </div>
+    );
+};
+
+export default Header;
